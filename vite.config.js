c3d9eae7bb1e9373d45eaa2c387e81e2a05d9ca7
Besim Gurbuz <contact@besimgurbuz.dev>
2026-01-11 21:16:27 +0100

    add psn-api to no externals in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+
+	ssr: {
+		noExternal: ['psn-api']
+	},
 
 	test: {
 		expect: { requireAssertions: true },
